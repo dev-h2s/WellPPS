26224c4b30405a04f8812af848547e2b0a5ad78a
Kang il, Seo <tylorstyle@gmail.com>
2022-06-27 14:03:28 +0900

    Repositories Init.

---
+++ src/main/groovy/com/wellnetworks/wellpps/WellPpsApplication.groovy
@@ -0,0 +1,13 @@
+package com.wellnetworks.wellpps
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WellPpsApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(WellPpsApplication, args)
+    }
+
+}
---
+++ src/test/groovy/com/wellnetworks/wellpps/WellPpsApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.wellnetworks.wellpps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WellPpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

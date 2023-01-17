738478b764ac43b05eddcc63b30269af2d10169b
sung029 <sung029@naver.com>
2023-01-17 17:42:48 +0900

    cors() header setting 전체 오픈

--- well-secure/src/main/kotlin/com/wellnetworks/wellsecure/config/SecurityConfig.kt
+++ well-secure/src/main/kotlin/com/wellnetworks/wellsecure/config/SecurityConfig.kt
@@ -13,8 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.cors.CorsConfiguration
 import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.CorsUtils
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
@@ -29,6 +31,7 @@ class SecurityConfig(
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity): SecurityFilterChain? {
         http
+            //.cors().configurationSource(corsConfigurationSource())
             .cors().and()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -36,10 +39,13 @@ class SecurityConfig(
 //            .formLogin().disable()
 //            .httpBasic().disable()
             .authorizeRequests()
+            //.requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
             .antMatchers("/api/**").permitAll()
             .antMatchers("/", "/**", "/init/**").permitAll()
             .antMatchers("/admin/hr/business/**").permitAll()
             .antMatchers("/file/**").permitAll()
+            .antMatchers("/login").permitAll()
+            .antMatchers("/**").permitAll() //그외 나머지 요청은 누구나 접근 가능
             .anyRequest().authenticated()
             .and()
             .addFilter(JwtAuthenticationFilter(authenticationManager, securityProperties, tokenProvider))
@@ -57,24 +63,31 @@ class SecurityConfig(
     fun corsConfigurationSource(): CorsConfigurationSource = UrlBasedCorsConfigurationSource().also {
         cors -> CorsConfiguration().apply {
             allowedOrigins = listOf("*")
+            //allowedOrigins = listOf("http://welldev.iptime.org:8888")
             allowedMethods = listOf("POST", "PUT", "DELETE", "GET", "OPTIONS", "HEAD")
-            allowedHeaders = listOf(
+            allowedHeaders = listOf("*"/*
                 "Authorization",
                 "Context-Type",
                 "X-Requested-With",
                 "Accept",
                 "Origin",
                 "Access-Control-Request-Method",
-                "Access-Control-Request-Headers"
+                "Access-Control-Request-Headers",
+                "Content-Type",
+                "Access-Control-Request-Credentials",
+                "Access-Control-Request-Private-Network"
+*/
             )
             exposedHeaders = listOf(
                 "Access-Control-Allow-Origin",
                 "Access-Control-Allow-Credentials",
                 "Authorization",
-                "Content-Disposition"
+                "Content-Disposition",
+                //"Access-Control-Allow-Private-Network"
             )
             maxAge = 3600
             cors.registerCorsConfiguration("/**", this)
         }
     }
+
 }

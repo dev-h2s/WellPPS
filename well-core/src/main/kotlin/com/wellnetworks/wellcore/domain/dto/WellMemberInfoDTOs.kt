9b06120ac658a327e191fc7921e47dbf068f4dc6
sung029 <sung029@naver.com>
2023-02-02 14:45:47 +0900

    사원관리 조회, 입력 jsonproperty 키 재설정

--- well-core/src/main/kotlin/com/wellnetworks/wellcore/domain/dto/WellMemberInfoDTOs.kt
+++ well-core/src/main/kotlin/com/wellnetworks/wellcore/domain/dto/WellMemberInfoDTOs.kt
@@ -1,9 +1,13 @@
 package com.wellnetworks.wellcore.domain.dto
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer
 import com.wellnetworks.wellcore.domain.enums.*
+import org.apache.coyote.Constants
 import org.hibernate.Hibernate
 import java.time.ZonedDateTime
 import java.util.*
@@ -12,51 +16,51 @@ import java.util.*
 data class WellMemberInfoDTO(
     @JsonProperty("idx")
     val Idx: String,
-    @JsonProperty("tid")
+    @JsonProperty("table_id")
     val Table_ID: String,
-    @JsonProperty("cure")
+    @JsonProperty("emp_current")
     val Current_Employment: CurrentEmploymentType,
     @JsonProperty("name")
     val Name: String,
     @JsonProperty("mail")
     val Email: String,
-    @JsonProperty("pphn")
+    @JsonProperty("phone_pri")
     val Phone_Private: String,
-    @JsonProperty("wphn")
+    @JsonProperty("phone_work")
     val Phone_Work: String,
-    @JsonProperty("regn")
+    @JsonProperty("jumin")
     val Registration_Number: String,
-    @JsonProperty("had1")
+    @JsonProperty("home_addr1")
     val Home_Address1: String,
-    @JsonProperty("had2")
+    @JsonProperty("home_addr2")
     val Home_Address2: String,
-    @JsonProperty("bnam")
+    @JsonProperty("bank_name")
     val Bank_Name: String,
-    @JsonProperty("bacc")
+    @JsonProperty("bank_account")
     val Bank_Account: String,
-    @JsonProperty("bhld")
+    @JsonProperty("bank_holder")
     val Bank_Holder: String,
-    @JsonProperty("dert")
+    @JsonProperty("dep")
     val Department: DepartmentType,
-    @JsonProperty("jpos")
+    @JsonProperty("pos")
     val Job_Position: JobPositionType,
-    @JsonProperty("lvl")
+    @JsonProperty("level")
     val Level: Byte,
-    @JsonProperty("emps")
+    @JsonProperty("emp_state")
     val Employment_State: EmploymentStateType,
-    @JsonProperty("jtyp")
+    @JsonProperty("emp_type")
     val Job_Type: JobType,
-    @JsonProperty("crtp")
+    @JsonProperty("phone_cert")
     val Certification_Phone: Boolean,
-    @JsonProperty("crte")
+    @JsonProperty("email_cert")
     val Certification_Email: Boolean,
-    @JsonProperty("entd")
+    @JsonProperty("entry_dt")
     val Entry_Datetime: ZonedDateTime,
-    @JsonProperty("epqd")
+    @JsonProperty("quit_dt")
     val Employment_Quit_Datetime: ZonedDateTime,
-    @JsonProperty("epqt")
+    @JsonProperty("quti_type")
     val Employment_Quit_Type: EmploymentQuitType,
-    @JsonProperty("accs")
+    @JsonProperty("access")
     val Access: Boolean,
 
     @JsonProperty("file1")
@@ -71,7 +75,8 @@ data class WellMemberInfoDTO(
     val Member_File5_idx: String,
 
     @JsonProperty("memo")
-    val Memo: String,
+    val Memo: String?,
+
     @JsonIgnore
     override val Modify_Datetime: ZonedDateTime?,
     @JsonIgnore
@@ -94,65 +99,65 @@ data class WellMemberInfoDTO(
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class WellMemberInfoDTOCreate(
     @JsonProperty("tid")
-    val Table_ID: String,
-    @JsonProperty("cure")
-    val Current_Employment: CurrentEmploymentType,
+    val Table_ID: String?,
+    @JsonProperty("emp_current")
+    val Current_Employment: CurrentEmploymentType, //소속회사
     @JsonProperty("name")
     val Name: String,
     @JsonProperty("mail")
-    val Email: String,
-    @JsonProperty("pphn")
-    val Phone_Private: String,
-    @JsonProperty("wphn")
-    val Phone_Work: String,
-    @JsonProperty("regn")
-    val Registration_Number: String,
-    @JsonProperty("had1")
-    val Home_Address1: String,
-    @JsonProperty("had2")
-    val Home_Address2: String,
-    @JsonProperty("bnam")
-    val Bank_Name: String,
-    @JsonProperty("bacc")
-    val Bank_Account: String,
-    @JsonProperty("bhld")
-    val Bank_Holder: String,
-    @JsonProperty("dert")
-    val Department: DepartmentType,
-    @JsonProperty("jpos")
-    val Job_Position: JobPositionType,
-    @JsonProperty("lvl")
-    val Level: Byte,
-    @JsonProperty("emps")
-    val Employment_State: EmploymentStateType,
-    @JsonProperty("jtyp")
-    val Job_Type: JobType,
-    @JsonProperty("crtp")
+    val Email: String?,
+    @JsonProperty("phone_pri")
+    val Phone_Private: String?,
+    @JsonProperty("phone_work")
+    val Phone_Work: String?,
+    @JsonProperty("jumin")
+    val Registration_Number: String?,
+    @JsonProperty("home_addr1")
+    val Home_Address1: String?,
+    @JsonProperty("home_addr2")
+    val Home_Address2: String?,
+    @JsonProperty("bank_name")
+    val Bank_Name: String?,
+    @JsonProperty("bank_account")
+    val Bank_Account: String?,
+    @JsonProperty("bank_holder")
+    val Bank_Holder: String?,
+    @JsonProperty("dep")
+    val Department: DepartmentType, //부서
+    @JsonProperty("pos")
+    val Job_Position: JobPositionType, //직책
+    @JsonProperty("level")
+    val Level: Byte?,
+    @JsonProperty("emp_status")
+    val Employment_State: EmploymentStateType, //재직상태
+    @JsonProperty("emp_type")
+    val Job_Type: JobType, //고용형태
+    @JsonProperty("phone_cert")
     val Certification_Phone: Boolean,
-    @JsonProperty("crte")
+    @JsonProperty("phone_cert")
     val Certification_Email: Boolean,
-    @JsonProperty("entd")
+    @JsonProperty("entry_dt")
     val Entry_Datetime: ZonedDateTime,
-    @JsonProperty("epqd")
+    @JsonProperty("quit_dt")
     val Employment_Quit_Datetime: ZonedDateTime,
-    @JsonProperty("epqt")
-    val Employment_Quit_Type: EmploymentQuitType,
-    @JsonProperty("accs")
+    @JsonProperty("quti_type")
+    val Employment_Quit_Type: EmploymentQuitType?,
+    @JsonProperty("access")
     val Access: Boolean,
 
     @JsonProperty("file1")
-    val Member_File1_idx: String,
+    val Member_File1_idx: String?,
     @JsonProperty("file2")
-    val Member_File2_idx: String,
+    val Member_File2_idx: String?,
     @JsonProperty("file3")
-    val Member_File3_idx: String,
+    val Member_File3_idx: String?,
     @JsonProperty("file4")
-    val Member_File4_idx: String,
+    val Member_File4_idx: String?,
     @JsonProperty("file5")
-    val Member_File5_idx: String,
+    val Member_File5_idx: String?,
 
     @JsonProperty("memo")
-    val Memo: String,
+    val Memo: String?,
     @JsonIgnore
     override val Modify_Datetime: ZonedDateTime?,
     @JsonIgnore

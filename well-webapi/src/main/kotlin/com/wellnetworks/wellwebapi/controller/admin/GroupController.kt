1e4b2bf83b1062a79fe053bbf0e21c30371a73ef
sung029 <sung029@naver.com>
2023-04-24 17:17:00 +0900

    사원관리 입력 >  key, value 조회 기능 구현 > Action, Menu

--- well-webapi/src/main/kotlin/com/wellnetworks/wellwebapi/controller/admin/GroupController.kt
+++ well-webapi/src/main/kotlin/com/wellnetworks/wellwebapi/controller/admin/GroupController.kt
@@ -3,11 +3,17 @@ package com.wellnetworks.wellwebapi.controller.admin
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.wellnetworks.wellcore.domain.dto.WellGroupDTO
+import com.wellnetworks.wellcore.domain.dto.WellPartnerDTO
 import com.wellnetworks.wellcore.domain.dto.WellPermissionDTO
+import com.wellnetworks.wellcore.domain.enums.CompanyType
+import com.wellnetworks.wellcore.domain.enums.MenuPermission
+import com.wellnetworks.wellcore.domain.enums.MenuPermissionAction
 import com.wellnetworks.wellcore.domain.enums.MenuPermissionUtil
 import com.wellnetworks.wellcore.service.WellGroupService
+import com.wellnetworks.wellwebapi.response.BaseItemRes
 import com.wellnetworks.wellwebapi.response.BaseListRes
 import com.wellnetworks.wellwebapi.response.BaseRes
+import com.wellnetworks.wellwebapi.response.ParamEnumItemRes
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Pageable
 import org.springframework.http.HttpStatus
@@ -104,5 +110,53 @@ class GroupController(private var groupService: WellGroupService) {
         }
 
         return ResponseEntity.ok(BaseRes(HttpStatus.OK, "삭제 성공"))
+    }
+
+    @GetMapping("group/menuAction")
+    @PreAuthorize("isAuthenticated() and" +
+            " (hasRole(T(com.wellnetworks.wellcore.domain.enums.PermissionKey).SUPER_ADMIN) or" +
+            " hasRole(T(com.wellnetworks.wellcore.domain.enums.PermissionKey).MEMBER))")
+    fun menuAction(): ResponseEntity<BaseListRes<ParamEnumItemRes>> {
+        var menuActionList : MutableList<ParamEnumItemRes> = mutableListOf()
+        for (item in MenuPermissionAction.asMap()) {
+            menuActionList.add(
+                ParamEnumItemRes(
+                item.hashCode(),
+                item.key,
+                item.toString()
+            )
+            )
+        }
+
+        return ResponseEntity.ok(
+            BaseListRes<ParamEnumItemRes>(
+                HttpStatus.OK, "",
+                menuActionList,
+            )
+        )
+    }
+
+    @GetMapping("group/menuPermission")
+    @PreAuthorize("isAuthenticated() and" +
+            " (hasRole(T(com.wellnetworks.wellcore.domain.enums.PermissionKey).SUPER_ADMIN) or" +
+            " hasRole(T(com.wellnetworks.wellcore.domain.enums.PermissionKey).MEMBER))")
+    fun menuPermission(): ResponseEntity<BaseListRes<ParamEnumItemRes>> {
+        var menuPermissionList : MutableList<ParamEnumItemRes> = mutableListOf()
+        for (item in MenuPermission.asMap()) {
+            menuPermissionList.add(
+                ParamEnumItemRes(
+                    item.hashCode(),
+                    item.key,
+                    item.toString()
+                )
+            )
+        }
+
+        return ResponseEntity.ok(
+            BaseListRes<ParamEnumItemRes>(
+                HttpStatus.OK, "",
+                menuPermissionList,
+            )
+        )
     }
 }

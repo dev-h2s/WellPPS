6de4d7dc6e5a2d21d802aafc2b68fac6ef7fcff7
sung029 <sung029@naver.com>
2023-05-23 17:09:20 +0900

    통신사 요금제 Entity 구현 작업중

---
+++ well-core/src/main/kotlin/com/wellnetworks/wellcore/domain/WellProductEntity.kt
@@ -0,0 +1,13 @@
+package com.wellnetworks.wellcore.domain
+
+import com.wellnetworks.wellcore.domain.converter.*
+import com.wellnetworks.wellcore.domain.enums.*
+import jakarta.persistence.Column
+import jakarta.persistence.Convert
+import jakarta.persistence.Id
+import org.hibernate.Hibernate
+import java.time.ZonedDateTime
+
+class WellProductEntity {
+    
+}

ece64b84cf59234c4946e5a9182da75838eb296b
kimjin9822 <kimjin9822@naver.com>
2023-09-27 09:58:14 +0900

    설정 컨트롤러 분석 및 주석

--- well-webapi/src/main/kotlin/com/wellnetworks/wellwebapi/controller/init/InitializeController.kt
+++ well-webapi/src/main/kotlin/com/wellnetworks/wellwebapi/controller/init/InitializeController.kt
@@ -21,6 +21,8 @@ import java.util.*
 @RestController
 @RequestMapping("/init")
 class InitializeController(private val userService: WellUserService, private val passwordEncoder: PasswordEncoder) {
+    // 관리자 생성
+    // 애플리케이션에 사용자가 아무도 등록되어 있지 않은 경우에만 실행
     @PostMapping("create_admin")
     fun createAdmin(@RequestBody createAdminReq: CreateAdminReq): ResponseEntity<BaseRes> {
         if (userService.dataTotalCount() == 0L) {
@@ -31,17 +33,19 @@ class InitializeController(private val userService: WellUserService, private val
             val createAdmin = WellUserDTOCreate(createAdminReq.username, null, permissions,
                 passwordEncoder.encode(createAdminReq.password))
             println(createAdmin)
-
+            // 관리자 생성 실패 시 FAILED_TO_SAVE_USER
             if (userService.createUser(createAdmin) == null)
             {
                 throw BaseException(BaseResponseCode.FAILED_TO_SAVE_USER)
             }
+            // http OK 관리자 생성
             return ResponseEntity.ok(BaseRes(HttpStatus.OK, "Create super admin complete."))
         }
-
+        // 생성 실패
         throw BaseException(BaseResponseCode.BAD_REQUEST)
     }
 
+    //  시스템 하드 리셋
     @GetMapping("hard_reset_system")
     fun hardresetSystem(): ResponseEntity<BaseRes> {
 

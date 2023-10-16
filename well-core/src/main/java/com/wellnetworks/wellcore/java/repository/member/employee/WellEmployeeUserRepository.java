78bfc037d3eb9f0850594971ff6ca33bc84b03d2
gugigugi92 <shsdev96@gmail.com>
2023-10-16 15:07:25 +0900

    오류 파일 주석 처리

--- well-core/src/main/java/com/wellnetworks/wellcore/java/repository/member/employee/WellEmployeeUserRepository.java
+++ well-core/src/main/java/com/wellnetworks/wellcore/java/repository/member/employee/WellEmployeeUserRepository.java
@@ -15,19 +15,19 @@ import java.util.Optional;
 public interface WellEmployeeUserRepository
         extends JpaRepository<WellEmployeeUserEntity, String>, JpaSpecificationExecutor<WellEmployeeUserEntity> {
     // optinal = null 참조 처리 가능 비어있어도 empty로 반환
-    // idx찾기
-    Optional<WellEmployeeUserEntity> findByemployeeId(String idx);
-
-    // 모든 정보 찾기
-    Page<WellEmployeeUserEntity> findAll(Pageable pageable);
-
-    // userId 찾기
-    Optional<WellEmployeeUserEntity> findByemployeeIdentification(String userID);
-
-    // userID 값이 존재하는지 여부 판단
-    boolean existsByemployeeIdentification(String userID);
-
-    // idx 삭제
-    Optional<Integer> deleteByemployeeIdx(String idx);
+//    // idx찾기
+//    Optional<WellEmployeeUserEntity> findByemployeeId(String idx);
+//
+//    // 모든 정보 찾기
+//    Page<WellEmployeeUserEntity> findAll(Pageable pageable);
+//
+//    // userId 찾기
+//    Optional<WellEmployeeUserEntity> findByemployeeIdentification(String userID);
+//
+//    // userID 값이 존재하는지 여부 판단
+//    boolean existsByemployeeIdentification(String userID);
+//
+//    // idx 삭제
+//    Optional<Integer> deleteByemployeeIdx(String idx);
 
 }
